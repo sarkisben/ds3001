aa591a915dca9d046adc99c697752703f580a459
Ben Sarkis <bgsarkis@gmail.com>
2018-04-29 15:52:42 -0400

    Round 2

--- js/dataAnalysis-twitterbubble.js
+++ js/dataAnalysis-twitterbubble.js
@@ -1,40 +1,42 @@
-var sayjohn = [
-                { "name": "Cabernet Sauvignon", "tweets": 86 },
-                { "name": "Red Blend", "tweets": 14 },
-                { "name": "Rose", "tweets": 200 },
-                { "name": "Pinot Noir", "tweets": 67 },
-                { "name": "Chardonnay", "tweets": 94 },
-                { "name": "Sauvignon Blanc", "tweets": 27 },
-                { "name": "Merlot", "tweets": 200 },
-                { "name": "Syrah", "tweets": 93 },
-                { "name": "Bordeaux-style Red Blend", "tweets": 5 },
-                { "name": "Riesling", "tweets": 200 }
-              ];
+var mayson = [{"Favorited": 94, "TotalTweets": 57, "Retweets": 92, "Wine": "Pinot Noir"}, 
+              {"Favorited": 206, "TotalTweets": 200, "Retweets": 1358, "Wine": "Chardonnay"}, 
+              {"Favorited": 207, "TotalTweets": 77, "Retweets": 353, "Wine": "Cabernet Sauvignon"}, 
+              {"Favorited": 24, "TotalTweets": 9, "Retweets": 6, "Wine": "Red Blend"}, 
+              {"Favorited": 3, "TotalTweets": 1, "Retweets": 0, "Wine": "Bordeaux-style Red Blend"}, 
+              {"Favorited": 290, "TotalTweets": 200, "Retweets": 409, "Wine": "Riesling"}, 
+              {"Favorited": 9, "TotalTweets": 10, "Retweets": 2, "Wine": "Sauvignon Blanc"}, 
+              {"Favorited": 266, "TotalTweets": 200, "Retweets": 986, "Wine": "Syrah"}, 
+              {"Favorited": 69, "TotalTweets": 200, "Retweets": 91736, "Wine": "Rose"}, 
+              {"Favorited": 805, "TotalTweets": 200, "Retweets": 2189, "Wine": "Merlot"}]
 
 // Create the bubble graph
 function makeBubbleChart(){
   //Get unique varieties and occurances
   var winenames = []
+  var favorites = []
   var tweets = []
-  // for (var i = 0; i < sayjohn.length; i++){
-  //   var obj = sayjohn[i];
-  //   for (var key in obj){
-  //    winenames.push(key);
-  //    tweets.push(obj[key]);
-  //   }
-  // }
-  sayjohn.forEach(function(d) {
-    winenames.push(d.name);
-    tweets.push(d.tweets);
+  var favoriteRetweets = []
+  var retweets = [];
+  mayson.forEach(function(d) {
+    winenames.push(d.Wine);
+    tweets.push(d.TotalTweets);
+    favorites.push(d.Favorited);
+    favoriteRetweets.push("Favorited "+d.Favorited+" times. </br>Retweeted "+d.Retweets+" times.");
+    retweets.push(d.Retweets);
   });
 
   //... adding to chart
   var trace = {
    x: winenames,
    y: tweets,
+   text: favoriteRetweets,
    mode: 'markers',
    marker: {
-     size: tweets
+     size: favorites,
+     sizemode: 'area',
+     sizeref: 0.15,
+     color: retweets,
+     colorscale: [[0, 'rgb(25, 150, 25)'], [1, 'rgb(150, 25, 150)']]
    }
   };
 
